e54eb29932226708cab56c28e36d9256158ce68c
vinaybommana7 <vinaybommana7@gmail.com>
2017-09-12 00:46:26 +0530

    added linked list implementation for adding a number at the beginning and end of the list

---
+++ LinkedLists/linkedlist.cpp
@@ -0,0 +1,75 @@
+// program name :  linked list implementation
+// date         :  Mon Sep 11 23:44:48 IST 2017
+// adding an element at the end of the list
+
+# include <iostream>
+# include <cstdio>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* link;
+};
+
+
+void printList(Node* head) {//{{{
+    cout << "The elements in the list are:" << endl;
+    while(head != NULL) {
+        cout << head -> data << " ";
+        head = head -> link;
+    }
+    cout << endl;
+}/*}}}*/
+
+
+void insertAtEnd(Node** head, int x) {
+    if(*head == NULL) {
+        Node* temp = new Node();
+        temp -> data = x;
+        *head = temp;
+    } else {
+        Node* temp = new Node();
+        temp -> data = x;
+        Node* temp1 = new Node();
+        temp1 = *head;
+        while(temp1 -> link != NULL) {
+            temp1 = temp1 -> link;
+        }
+        temp1 -> link = temp;
+    }
+}
+
+void insertAtBeginning(Node** head, int x) {
+    Node* temp = new Node();
+    temp -> data = x;
+    if(*head == NULL) {
+        *head = temp;
+    } else {
+        temp -> link = *head;
+        *head = temp;
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    Node* head = NULL;
+    int n;
+    cout << "Enter how many elements you want to add:" << endl;
+    cin >> n;
+    for(int i = 0; i < n; ++i) {
+        int x;
+        char option;
+        cout << "Enter number: ";
+        cin >> x;
+        cout << "Where do you want to add the element:(end(e)/beginning(b))" << endl;
+        cin >> option;
+        if(option == 'e') {
+            insertAtEnd(&head, x);
+        } else {
+            insertAtBeginning(&head, x);
+        }
+    }
+    cout << endl;
+    printList(head);
+}
